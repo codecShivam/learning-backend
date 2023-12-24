181f5524ffb246ef8b79774d460e342c7c11a21d
codecShivam <codecshivam@gmail.com>
2023-12-25 04:45:54 +0530

    server build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,6 +10,14 @@ app.get("/twitter", (req, res) => {
   res.send("codecShivam");
 });
 
-app.listen(port, () => {
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login first!!</h1>");
+});
+
+app.get("/shivam", (req, res) => {
+  res.send('<a href="https://codecshivam.netlify.app">Shivam</a>');
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
